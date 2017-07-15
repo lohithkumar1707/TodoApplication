4b8b7ae6450ddc90a2e80748457694ddfa5f9bf9
lohithkumar1707 <lohithkumar.r@ladybirdweb.com>
2017-07-15 14:17:02 +0530

    'ok'

--- multiauth1/resources/views/layouts/app.blade.php
+++ multiauth1/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Todo Application</title>
+    <title>Todo App</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
